1965d88e43bb623f95fbd7951d36e40efa101b6a
Micah Banschick <micha.ban@gmail.com>
2020-01-23 19:19:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(arr)# code an argument here
   # Your code here
   #narr = []
-  arr.each_with_index {|name,index| puts "#{index} #{name}"}
+  arr.each_with_index {|name,index| puts "#{index + 1} #{name}"}
 end
 
 def summon_captain_planet# code an argument here
