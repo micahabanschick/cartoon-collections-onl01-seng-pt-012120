6b174738b5abd3cf2ec035e4f5153f9ce3b2a0d7
Micah Banschick <micha.ban@gmail.com>
2020-01-23 19:20:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,15 @@
-def roll_call_dwarves(arr)# code an argument here
-  # Your code here
-  #narr = []
+def roll_call_dwarves(arr)
   arr.each_with_index {|name,index| puts "#{index + 1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet
+end
+
+def long_planeteer_calls
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
