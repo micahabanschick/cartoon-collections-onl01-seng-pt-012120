97f27b6e2764fd0500e236c273134c731a3a4790
Micah Banschick <micha.ban@gmail.com>
2020-01-23 19:29:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.find {|word| cheese_types.includes?(word)}
+  arr.find {|word| cheese_types.include?(word)}
 end
